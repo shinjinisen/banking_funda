62f3a42a9fc1ff1e3e372045c2fab064cfec92a0
Shinjini Sen <98477105+shinjinisen@users.noreply.github.com>
2023-10-15 14:12:42 +0530

    Update Transfer.jsx

--- src/token_assets/src/components/Transfer.jsx
+++ src/token_assets/src/components/Transfer.jsx
@@ -1,67 +1,48 @@
-import React, { useState } from "react";
-import {Principal} from "@dfinity/principal";
-import {token} from "../../../declarations/token";
-function Transfer() {
-  
-  const [recipientId, setId]=useState("");
-  const [amount,setAmount]=useState("");
-  const [isDisabled, setDisabled]=useState(false);
-  const [feedback, setFeedback]=useState("");
-  const [isHidden,setHidden]=useState(true);
+import React, {useState}from "react";
+import {token, canisterId, createActor} from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
 
-  async function handleClick() {
-    setHidden(true);
+function Faucet(props) {
+  const [isDisabled, setDisabled]=useState(false);
+  const [buttonText, setText]=useState("Gimme gimme");
+
+  async function handleClick(event) {
     setDisabled(true);
-    const recipient=Principal.fromText(recipientId);
-    const amountToTransfer=Number(amount);
-    const result= await token.transfer(recipient,amountToTransfer);
-    setFeedback(result);
-    setHidden(false);
-    setDisabled(false);
+    
+    const authClient=await AuthClient.create();
+    const identity= await authClient.getIdentity();
+
+    const authenticatedCanister=createActor(canisterId, {
+      agentOptions:{
+        identity,
+      },
+    });
+
+    const result= await authenticatedCanister.payOut();
+    setText(result);
+    // setDisabled(false);
   }
 
   return (
-    <div className="window white">
-      <div className="transfer">
-        <fieldset>
-          <legend>To Account:</legend>
-          <ul>
-            <li>
-              <input
-                type="text"
-                id="transfer-to-id"
-                value={recipientId}
-                onChange={(e)=>setId(e.target.value)}
-              />
-            </li>
-          </ul>
-        </fieldset>
-        <fieldset>
-          <legend>Amount:</legend>
-          <ul>
-            <li>
-              <input
-                type="number"
-                id="amount"
-                value={amount}
-                onChange={(e)=>setAmount(e.target.value)}
-              />
-            </li>
-          </ul>
-        </fieldset>
-        <p className="trade-buttons">
-          <button 
-          id="btn-transfer" 
-          onClick={handleClick}
-          disabled={isDisabled}
-          >
-            Transfer
-          </button>
-        </p>
-        <p hidden={isHidden}>{feedback}</p>
-      </div>
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free SHINX tokens here! Claim 10,000 SHINX coins to {props.userPrincipal} .</label>
+      <p className="trade-buttons">
+        <button 
+        id="btn-payout" 
+        onClick={handleClick}
+        disabled={isDisabled}
+        >
+          {buttonText}
+        </button>
+      </p>
     </div>
   );
 }
 
-export default Transfer;
+export default Faucet;
