abca70001cfca6fd938b89f1e1d6206850a50822
Shinjini Sen <98477105+shinjinisen@users.noreply.github.com>
2023-10-15 13:50:47 +0530

    Update Faucet.jsx
    deploying into live IC network

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,5 +1,7 @@
 import React, {useState}from "react";
-import {token} from "../../../declarations/token";
+import {token, canisterId, createActor} from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
+
 
 function Faucet() {
   const [isDisabled, setDisabled]=useState(false);
@@ -7,7 +9,17 @@ function Faucet() {
 
   async function handleClick(event) {
     setDisabled(true);
-    const result= await token.payOut();
+    
+    const authClient=await AuthClient.create();
+    const identity= await authClient.getIdentity();
+
+    const authenticatedCanister=createActor(canisterId, {
+      agentOptions:{
+        identity,
+      },
+    });
+
+    const result= await authenticatedCanister.payOut();
     setText(result);
     // setDisabled(false);
   }
