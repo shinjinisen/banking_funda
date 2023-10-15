1196191739675624c08aaa41bc56385e72ef8291
Shinjini Sen <98477105+shinjinisen@users.noreply.github.com>
2023-10-15 14:16:05 +0530

    Update Transfer.jsx

--- src/token_assets/src/components/Transfer.jsx
+++ src/token_assets/src/components/Transfer.jsx
@@ -1,48 +1,78 @@
-import React, {useState}from "react";
-import {token, canisterId, createActor} from "../../../declarations/token";
-import { AuthClient } from "@dfinity/auth-client";
-
-function Faucet(props) {
+import React, { useState } from "react";
+import {Principal} from "@dfinity/principal";
+import {token} from "../../../declarations/token";
+import {AuthClient} from "@dfinity/auth-client";
+import { canisterId } from "../../../declarations/token/index";
+function Transfer() {
+  
+  const [recipientId, setId]=useState("");
+  const [amount,setAmount]=useState("");
   const [isDisabled, setDisabled]=useState(false);
-  const [buttonText, setText]=useState("Gimme gimme");
+  const [feedback, setFeedback]=useState("");
+  const [isHidden,setHidden]=useState(true);
 
-  async function handleClick(event) {
+  async function handleClick() {
+    setHidden(true);
     setDisabled(true);
+    const recipient=Principal.fromText(recipientId);
+    const amountToTransfer=Number(amount);
     
     const authClient=await AuthClient.create();
     const identity= await authClient.getIdentity();
-
     const authenticatedCanister=createActor(canisterId, {
       agentOptions:{
         identity,
-      },
+      }
     });
-
-    const result= await authenticatedCanister.payOut();
-    setText(result);
-    // setDisabled(false);
+    
+    const result= await token.transfer(recipient,amountToTransfer);
+    setFeedback(result);
+    setHidden(false);
+    setDisabled(false);
   }
 
   return (
-    <div className="blue window">
-      <h2>
-        <span role="img" aria-label="tap emoji">
-          🚰
-        </span>
-        Faucet
-      </h2>
-      <label>Get your free SHINX tokens here! Claim 10,000 SHINX coins to {props.userPrincipal} .</label>
-      <p className="trade-buttons">
-        <button 
-        id="btn-payout" 
-        onClick={handleClick}
-        disabled={isDisabled}
-        >
-          {buttonText}
-        </button>
-      </p>
+    <div className="window white">
+      <div className="transfer">
+        <fieldset>
+          <legend>To Account:</legend>
+          <ul>
+            <li>
+              <input
+                type="text"
+                id="transfer-to-id"
+                value={recipientId}
+                onChange={(e)=>setId(e.target.value)}
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend>Amount:</legend>
+          <ul>
+            <li>
+              <input
+                type="number"
+                id="amount"
+                value={amount}
+                onChange={(e)=>setAmount(e.target.value)}
+              />
+            </li>
+          </ul>
+        </fieldset>
+        <p className="trade-buttons">
+          <button 
+          id="btn-transfer" 
+          onClick={handleClick}
+          disabled={isDisabled}
+          >
+            Transfer
+          </button>
+        </p>
+        <p hidden={isHidden}>{feedback}</p>
+      </div>
     </div>
   );
 }
 
-export default Faucet;
+export default Transfer;
